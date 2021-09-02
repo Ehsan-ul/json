89ed5a544a3e2d2e15b25b371aea2c076357568b
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-09-03 04:26:39 +0600

    add index.html

--- json-main.js
+++ json-main.js
@@ -1,3 +1,6 @@
+// AJAX code
+// browser e console kore dekhte hobe
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
